dcc62815d6ffe80df4cb549db39a26729719a504
Junuu <37577891+Junuu@users.noreply.github.com>
2019-11-19 13:14:37 +0900

    db data 불러오기

--- main.js
+++ main.js
@@ -3,21 +3,17 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 var path = require('path');
-const Module_template = require('./shopping_mall.js');
-
 var mysql = require('mysql');
+const Module_template = require('./shopping_mall.js');
 
 var db = mysql.createConnection({
   host      : 'localhost',
   user      : 'root',
-  password  : 'Your PassWord',
+  password  : 'your password',
   database : 's_malldb'
 });
 db.connect();
-
-db.query(`SELECT * FROM notice`, function(error,topics){
-  console.log(topics);
-});
+//db.end();
 
 var server = http.createServer(function(request, response) {
   var _url = request.url;
@@ -30,58 +26,131 @@ var server = http.createServer(function(request, response) {
   var active_shopping_basket="";
   var active_order_tracking="";
   var active_faq="right";
-
-  console.log(pathname);
+  var description = '';
+  var template = '';
+  var imgaddress = '';
+  //console.log(pathname); 에러확인을위한 코드
 
   if(pathname ==='/' || pathname ==='/shopping_mall.css' || pathname ==='/shopping_mall.js' || pathname ==='/kakao_login.js')
   {
-  fs.readFile(`data/${queryData.id}`,'utf8', function(err,data){
-    var description = data;
 
-    if(queryData.id === undefined)
-    {
-      title = 'Home';
-      active_home = "active";
-    }
-    else if(queryData.id === 'product')
-    {
-      active_product = "active";
-    }
-    else if(queryData.id === 'shopping_basket')
-    {
-      active_shopping_basket ="active";
-    }
-    else if(queryData.id === 'order_tracking')
-    {
-      active_order_tracking = "active";
-    }
-    else if(queryData.id === 'faq')
-    {
-      active_faq = "right_active" ;
-    }
-    else if(queryData.id === 'login'){
-      description = "로그인 완료";
-    }
-    else{//queryData.id ==='logout'
-      description = "로그아웃 완료";
-    }
+      if(queryData.id === undefined)
+      {
+        db.query(`SELECT * FROM notice `, function(error,topics){
+          if(error)
+          {
+            throw error;
+          }
+          title = 'Home';
+          active_home = "active";
+          for(var i=0;i<topics.length;i++)
+          {
+            temp_str = topics[i].upload_date + ""; // add "" to make string type
+            description += "<br>" +topics[i].title + " " + topics[i].content + " " + temp_str.slice(0,10);
+          }
+          template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          response.end(template);
+        });
+      }
+      else if(queryData.id === 'product')
+      {
+        db.query(`SELECT * FROM product`,function(error,products){
+          if(error)
+          {
+            throw error;
+          }
+          active_product = "active";
+          for(var i=0;i<products.length;i++)
+          {
+            description += "<br>" +products[i].p_name + " " + products[i].price + " "+ products[i].info;
+          }
+          for(var i=0;i<products.length;i++)
+          {
+            console.log(products[i].image);
+          }
+          template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          response.end(template);
 
-    if(ext){
-    if(ext ==='.css'){
-     response.writeHead(200, {'Content-Type': 'text/css'});
-    }
-    else if(ext === '.js'){
-     response.writeHead(200, {'Content-Type': 'text/javascript'});
-    }
-     response.write(fs.readFileSync(__dirname + pathname, 'utf8'));
-    }
-    else{
-     response.writeHead(200, {'Content-Type': 'text/html'});
+        });
 
-    }
-    var template= Module_template.HTML(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
-    response.end(template);
-   })
+      }
+      else if(queryData.id === 'shopping_basket')
+      {
+        db.query(`SELECT * FROM shopping_basket`,function(error,baskets){
+          if(error)
+          {
+            throw error;
+          }
+          active_shopping_basket ="active";
+          for(var i=0;i<baskets.length;i++)
+          {
+            description += "<br>" +baskets[i].id + " " + baskets[i].p_name + " "+ baskets[i].p_count;
+          }
+          template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          response.end(template);
+        });
+      }
+
+      else if(queryData.id === 'order_tracking')
+      {
+        db.query(`SELECT * FROM order_tracking`,function(error,trackings){
+          if(error)
+          {
+            throw error;
+          }
+          active_order_tracking = "active";
+          for(var i=0;i<trackings.length;i++)
+          {
+            description += "<br>" +trackings[i].tracking_num + " " + trackings[i].id + " " + trackings[i].order_state + " "+ trackings[i].invoice_number;
+          }
+          template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          response.end(template);
+        });
+      }
+
+      else if(queryData.id === 'faq')
+      {
+        db.query(`SELECT * FROM faq`,function(error,faqs){
+          if(error)
+          {
+            throw error;
+          }
+          active_faq = "right_active" ;
+          for(var i=0;i<faqs.length;i++)
+          {
+            temp_str = faqs[i].upload_date + ""; // add "" to make string type
+            description += "<br>" +faqs[i].title + " " + faqs[i].content + " " + temp_str.slice(0,10);
+          }
+          template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          response.end(template);
+        });
+      }
+      else if(queryData.id === 'login'){
+        description = "로그인 완료";
+        template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+        response.end(template);
+      }
+      else{//queryData.id ==='logout'
+        description = "로그아웃 완료";
+        template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+        response.end(template);
+      }
+
+
+      if(ext){
+        if(ext ==='.css'){
+         response.writeHead(200, {'Content-Type': 'text/css'});
+        }
+        else if(ext === '.js'){
+         response.writeHead(200, {'Content-Type': 'text/javascript'});
+        }
+         response.write(fs.readFileSync(__dirname + pathname, 'utf8'));
+      }
+      else{
+       response.writeHead(200, {'Content-Type': 'text/html'});
+      }
+
+
  }
  else
  {
