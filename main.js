3cfe4ff22f17813cda62a35ee39c3f1777588b0d
Junuu <37577891+Junuu@users.noreply.github.com>
2019-11-22 16:54:58 +0900

    상품이미지 추가 및 객체화
    제품부분의 이미지 추가 및 객체화

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 var path = require('path');
-var mysql = require('mysql');
 const Module_template = require('./shopping_mall.js');
+var mysql = require('mysql');
 
 var db = mysql.createConnection({
   host      : 'localhost',
   user      : 'root',
-  password  : 'your password',
+  password  : '11111111',
   database : 's_malldb'
 });
 db.connect();
@@ -33,7 +33,6 @@ var server = http.createServer(function(request, response) {
 
   if(pathname ==='/' || pathname ==='/shopping_mall.css' || pathname ==='/shopping_mall.js' || pathname ==='/kakao_login.js')
   {
-
       if(queryData.id === undefined)
       {
         db.query(`SELECT * FROM notice `, function(error,topics){
@@ -60,15 +59,19 @@ var server = http.createServer(function(request, response) {
             throw error;
           }
           active_product = "active";
+          var product_array = new Array();
           for(var i=0;i<products.length;i++)
           {
-            description += "<br>" +products[i].p_name + " " + products[i].price + " "+ products[i].info;
+            product_array[i] =
+            {
+              p_name : products[i].p_name,
+              p_price : products[i].price,
+              p_info : products[i].info
+            };            
+            //product_array[i] = "제품명:" + products[i].p_name + " 가격:" + products[i].price + " 상품정보:" + products[i].info;
           }
-          for(var i=0;i<products.length;i++)
-          {
-            console.log(products[i].image);
-          }
-          template= Module_template.BeforeLogin(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          template= Module_template.Product(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,product_array);
+
           response.end(template);
 
         });
@@ -154,6 +157,10 @@ var server = http.createServer(function(request, response) {
  }
  else
  {
+   // 여기다가 index.html이 아닌 다른애들을 호출할 수 있다.
+   //else if(pathname == '/unnamed.html'){
+
+ //}
     response.writeHead(404);
     response.end('not Found');
  }
