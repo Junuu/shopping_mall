9fbf7b77f1014c288fb0caea8741fb4618976a51
Junuu <37577891+Junuu@users.noreply.github.com>
2019-12-18 17:15:02 +0900

    UI 부분 개선

--- main.js
+++ main.js
@@ -44,11 +44,13 @@ var server = http.createServer(function(request, response) {
           }
           title = 'Home';
           active_home = "active";
+          description = "<h1>공지사항</h1><ul><li>내용<span>날짜</span></li>";
           for(var i=0;i<topics.length;i++)
           {
             temp_str = topics[i].upload_date + ""; // add "" to make string type
-            description += "<br>" +topics[i].title + " " + topics[i].content + " " + temp_str.slice(0,10);
+            description += "<li><a>" +topics[i].content + "</a><span>"+ temp_str.slice(0,10) + "</span></li>";
           }
+          description += "</ul>" ;
           template= Module_template.Home(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
           response.end(template);
         });
@@ -91,7 +93,7 @@ var server = http.createServer(function(request, response) {
           {
             description += "<br>" +baskets[i].id + " " + baskets[i].p_name + " "+ baskets[i].p_count;
           }
-          template= Module_template.Home(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          template= Module_template.shopping_basket(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
           response.end(template);
         });
       }
@@ -116,6 +118,7 @@ var server = http.createServer(function(request, response) {
 
               track_info += "아이디 : " + save_id + "&emsp;&emsp;배송상태 : " + trackings[i].order_state + "&emsp;&emsp;운송장 번호 : " + trackings[i].invoice_number + "<br>";
             }
+            // &emsp; = 공백 4개
             else
             {
               count = count + 1;
@@ -150,12 +153,15 @@ var server = http.createServer(function(request, response) {
             throw error;
           }
           active_faq = "right_active" ;
+          description = "<h1>FAQ</h1><ul><li>질문<span>답변</span></li>";
           for(var i=0;i<faqs.length;i++)
           {
             temp_str = faqs[i].upload_date + ""; // add "" to make string type
-            description += "<br>" +faqs[i].title + " " + faqs[i].content + " " + temp_str.slice(0,10);
+            description += "<li><a>" +faqs[i].title + "</a><span>"+ faqs[i].content + "</span></li>";
+            //+ temp_str.slice(0,10);
           }
-          template= Module_template.Home(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
+          description += "</ul>" ;
+          template= Module_template.faq(active_home,active_product,active_shopping_basket,active_order_tracking,active_faq,title,description);
           response.end(template);
         });
       }
